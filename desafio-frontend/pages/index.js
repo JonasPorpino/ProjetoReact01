a86c6e456b0320cbb1b796b883a5daf9af65ff3e
JonasPorpino <49802323+JonasPorpino@users.noreply.github.com>
2024-04-05 16:23:45 -0300

    nav

--- desafio-frontend/pages/index.js
+++ desafio-frontend/pages/index.js
@@ -1,9 +1,21 @@
 import Box from "@/components/Box";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 export default function Home() {
   return (
     <>
-      <div className="container">
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="container mt-3">
         <Box titulo="Tomás Turbando">
           <p>Um parágrafo</p>
           <p>Outro parágrafo</p>
@@ -12,6 +24,7 @@ export default function Home() {
         <Box titulo="Oscar Alho">
           <p>Um parágrafo</p>
           <p>Outro parágrafo</p>
+          <button>Não sei</button>
         </Box>
           
         <Box titulo="Cuca Beludo">
