a3b845e246cc78b3286982ed2a6973e3ae01a566
JonasPorpino <49802323+JonasPorpino@users.noreply.github.com>
2024-04-14 12:02:46 -0300

    api viacep

--- desafio-frontend/pages/enderecos.js
+++ desafio-frontend/pages/enderecos.js
@@ -1,8 +1,37 @@
 import Pagina from '@/components/Pagina'
+import axios from 'axios'
 import React from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
 
 const enderecos = () => {
+
+  const {register, handleSubmit, setValue} = useForm()
+
+  function buscarCep(event){
+    const cep = event.target.value
+
+    if(cep.length == 8){
+
+      axios.get('https://viacep.com.br/ws/'+cep+'/json/').then(resultado =>{
+        const endereco = resultado.data
+
+        setValue('uf',endereco.uf)
+        setValue('cidade',endereco.localidade)
+        setValue('bairro',endereco.bairro)
+        setValue('logradouro',endereco.logradouro)
+        setValue('complemento',endereco.complemento)
+
+
+      })
+
+    }
+  }
+
+
+  function salvar(dados){
+    console.log(dados)
+  }
   return (
     <Pagina>
       <h3>Endereço</h3>
@@ -10,34 +39,39 @@ const enderecos = () => {
 
             <Form.Group className="mb-3" controlId="cep">
               <Form.Label>CEP:</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" {...register('cep')} onChange={buscarCep}/>
             </Form.Group>
             
             <Form.Group className="mb-3" controlId="uf">
               <Form.Label>UF:</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" {...register('uf')}/>
+            </Form.Group>
+            
+            <Form.Group className="mb-3" controlId="cidade">
+              <Form.Label>Cidade:</Form.Label>
+              <Form.Control type="text" {...register('cidade')}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="bairro">
               <Form.Label>Bairro:</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" {...register('bairro')}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="logradouro">
               <Form.Label>Logradouro:</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" {...register('logradouro')}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="complemento">
               <Form.Label>Complemento:</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" {...register('complemento')}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="numero">
               <Form.Label>Número:</Form.Label>
-              <Form.Control type="text" />
+              <Form.Control type="text" {...register('numero')}/>
             </Form.Group>
-            <Button variant="info">
+            <Button variant="info" onClick={handleSubmit(salvar)}>
                   Enviar
             </Button>
         </Form>
